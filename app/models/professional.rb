7ee22e776b9b2bab820ba960d73b1db0d0ef8f2e
Pati Demitroff <patidemitroff@gmail.com>
2017-11-20 16:46:21 -0200

    added professionals controller and vinculated it to an user

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -1,4 +1,4 @@
 class Professional < ApplicationRecord
   belongs_to :user
-  validates: :full_name, :profession, :birthday, :professional_number presence: true
+  validates :full_name, :profession, :birthday, :professional_number, presence: true, on: :update
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :professional
+  after_save :create_professional
+
+  # def google authentication
+  #TODO:incluir google auth code
+  #end
+
+
+  private
+
+  def create_professional
+    professional = Professional.new
+    professional.user = self
+    professional.save
+  end
 end
