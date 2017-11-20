c1fe52c48f356a442b6d959105523693dcd521e2
lucas furtado <lambada46@gmail.com>
2017-11-20 15:02:18 -0200

    view for new patient with simple_form

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,34 +1,4 @@
 class Patient < ApplicationRecord
-  before_action :set_patient, only: [:show, :edit]
-
-  def index
-    @patients = Patient.all
-  end
-
-  def new
-    @patient = Patient.new
-  end
-
-  def create
-    @patient = Patient.new(patient_params)
-    @patient.save
-  end
-
-  def show
-  end
-
-  def edit
-    @patient.update(patient_params)
-  end
-
-  private
-
-  def patient_params
-    params.require(:patient).permit(:full_name, :birthday, :sus_number, :gender, :address, :prontuario_number )
-  end
-
-  def set_patient
-    @patient = Patient.find(params[:id])
-  end
+  enum gender: [ :Masculino, :Feminino, :Outros ]
 
 end
