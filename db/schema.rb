9b1d3bf62ad3646f98a353189eecbc72e38b999b
caroline peron <carolineperon@gmail.com>
2017-11-20 15:00:02 -0200

    professional model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120140459) do
+ActiveRecord::Schema.define(version: 20171120163901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "professionals", force: :cascade do |t|
+    t.string "full_name"
+    t.string "professional_number"
+    t.string "photo"
+    t.date "birthday"
+    t.string "profession"
+    t.string "sus_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
