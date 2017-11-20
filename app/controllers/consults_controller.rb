7776e625c1779791be0eefef14350905f177cf94
lucas furtado <lambada46@gmail.com>
2017-11-20 15:49:20 -0200

    actions new and create with its routes

--- app/controllers/consults_controller.rb
+++ app/controllers/consults_controller.rb
@@ -1,2 +1,25 @@
 class ConsultsController < ApplicationController
+
+  def new
+    @consult = Consult.new
+  end
+
+  def create
+    @consult = Consult.new(consult_params)
+    @consult.patient = Patient.find(params[:patient_id])
+    @consult.professional = current_user.professional
+    if @consult.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def consult_params
+    params.require(:consult).permit( :date, :turn, :unit_cnes, :team_number, :place)
+  end
+
 end
