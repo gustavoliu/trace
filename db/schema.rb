9ca4575bad762a6a841052d2a90ce828e82b3125
lucas furtado <lambada46@gmail.com>
2017-11-21 13:54:05 -0200

    created attributes unit_cnes and team_number to professionals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121133722) do
+ActiveRecord::Schema.define(version: 20171121155246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 20171121133722) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "unit_cnes"
+    t.string "team_number"
     t.index ["user_id"], name: "index_professionals_on_user_id"
   end
 
