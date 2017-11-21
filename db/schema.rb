61b51293fb89f192a98aded01ea5135ef9fdd287
lucas furtado <lambada46@gmail.com>
2017-11-21 16:18:15 -0200

    migrations to change type of exams and refering, so that we can use enum for options

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121180852) do
+ActiveRecord::Schema.define(version: 20171121181607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,11 +72,11 @@ ActiveRecord::Schema.define(version: 20171121180852) do
   create_table "soaps", force: :cascade do |t|
     t.string "complaint"
     t.string "diagnosis"
-    t.string "exams"
-    t.string "refering"
     t.string "consult_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "place"
+    t.integer "turn"
   end
 
   create_table "users", force: :cascade do |t|
