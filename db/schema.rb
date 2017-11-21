8fddb2076fb0865a0cf3642a51989f49958e770a
lucas furtado <lambada46@gmail.com>
2017-11-21 16:25:21 -0200

    error corrections

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121181607) do
+ActiveRecord::Schema.define(version: 20171121182309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,8 +75,8 @@ ActiveRecord::Schema.define(version: 20171121181607) do
     t.string "consult_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "place"
-    t.integer "turn"
+    t.integer "exams"
+    t.integer "refering"
   end
 
   create_table "users", force: :cascade do |t|
