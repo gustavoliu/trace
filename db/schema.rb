c65fbee93a723dffd2b924a64410d9c76e1d4c5b
Gustavo Liu <gustavoliu@gmail.com>
2017-11-23 15:08:07 -0200

    Edit consultation link always edits same consultation

--- db/schema.rb
+++ db/schema.rb
@@ -37,8 +37,6 @@ ActiveRecord::Schema.define(version: 20171121182309) do
     t.string "team_number"
     t.integer "professional_id"
     t.integer "patient_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "place"
     t.integer "turn"
   end
