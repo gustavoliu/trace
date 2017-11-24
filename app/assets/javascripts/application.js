b802a6707bfc193e8d44407d740c6d057a5705ab
Pati Demitroff <patidemitroff@gmail.com>
2017-11-24 13:01:59 -0200

    new homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,3 +5,11 @@
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
 //= require_tree .
+
+$(document).ready(function() {
+  $(".rotate").textrotator({
+    animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+    separator: "|", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+    speed: 4000 // How many milliseconds until the next word show.
+  });
+});
