8598b9e59c7ca4ee8a17e885ed2c07b17ab22ddf
Pati Demitroff <patidemitroff@gmail.com>
2017-11-27 14:51:31 -0200

    added reports model

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,23 @@
 class ReportsController < ApplicationController
-  def get_consults
+
+  def self get_consults
+    #TODO: selecionar quantidade de consultas por tempo
+    #selecionar consultas do periodo
+    @consults = Consult.where(consult_date: selected_month)
+    #contar as consultas obtidas
+    return @consults.count
   end
 
-  def get_diagnosis
+  def self get_diagnosis
+    #TODO: selecionar quantidade de diagnosticos em um periodo de tempo
+    #selecionar 10 diagnosticos mais registrados no periodo
+    @consults = Consult.where(consult_date: selected_month)
+    @diagnosis = Soap.select([:diagnosis, consult_id: consults]).group(:created_at).first(10)
+    return @diagnosis
+    #olhar consultas e SOAPS do periodo
+    #contar diagnosticos registrados
+    #retornar os 10 com maior numero de registros (count)
   end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124154449) do
+ActiveRecord::Schema.define(version: 20171127165027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,15 +60,6 @@ ActiveRecord::Schema.define(version: 20171124154449) do
     t.integer "gender"
   end
 
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text "content"
-    t.string "searchable_type"
-    t.bigint "searchable_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
-  end
-
   create_table "professionals", force: :cascade do |t|
     t.string "full_name"
     t.string "professional_number"
@@ -83,6 +74,11 @@ ActiveRecord::Schema.define(version: 20171124154449) do
     t.string "team_number"
     t.string "google_photo"
     t.index ["user_id"], name: "index_professionals_on_user_id"
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "soaps", force: :cascade do |t|
