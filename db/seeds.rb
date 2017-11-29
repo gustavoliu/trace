67df7b3c9b3083d7deb34003352e61eb80adb1e2
Gustavo Liu <gustavoliu@gmail.com>
2017-11-29 15:38:08 -0200

    Seed creates diseases now

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,25 @@
 require 'csv'
-# puts "Importando diseases"
-# Disease.destroy_all
 
+puts "Importando diseases"
+Disease.destroy_all
 
-# csv_text = File.read('CIAP2.csv')
-# csv = CSV.parse(csv_text, {col_sep: ';'})
-# csv.each do |row|
-#   d = Disease.new
-#   d.ciap_code = row[0]
-#   d.formal_name = row[1]
-#   d.pop_name = row[2]
-#   d.is_diagnosis = row[3]
-#   d.save
-# end
+csv_text = File.read('CIAP2.csv')
+csv = CSV.parse(csv_text, {col_sep: ';'})
+csv.each do |row|
+  d = Disease.new
+  d.ciap_code = row[0]
+  d.formal_name = row[1]
+  d.pop_name = row[2]
+  d.is_diagnosis = row[3]
+  d.save
+end
 
 # 50.times do
 #   Patient.create({full_name: Faker::Name.name,
 #     sus_number: Faker::Number.number(5)})
 # end
 
-puts "Importanto pacientes"
+puts "Importando pacientes"
 Patient.destroy_all
 
 csv_patient = File.read('SEED.csv')
